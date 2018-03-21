93d1b2d0c5d1048e9bfe8bd6b02009a25fddd93e
Tom Lant <lampholder@gmail.com>
2018-03-21 16:02:37 +0000

    Make params mandatory

--- scripts/register_matrix_user.py
+++ scripts/register_matrix_user.py
@@ -7,23 +7,19 @@ import argparse
 from concorde.integrations import Matrix
 
 parser = argparse.ArgumentParser(description='Register matrix users with a Matrix homeserver')
-parser.add_argument('--homeserver')
-parser.add_argument('--homeserver-secret')
-parser.add_argument('--passgen-secret')
+parser.add_argument('--homeserver', required=True)
+parser.add_argument('--homeserver-secret', required=True)
+parser.add_argument('--passgen-secret', required=True)
 parser.add_argument('mxids', nargs='*')
 args = parser.parse_args()
 
-homeserver = args.homeserver
 homeserver_secret = args.homeserver_secret
 passgen_secret = args.passgen_secret
 mxids = args.mxids
 
-assert homeserver is not None
-assert homeserver_secret is not None
-assert passgen_secret is not None
 assert len(mxids) > 0
 
-matrix = Matrix(homeserver)
+matrix = Matrix(args.homeserver)
 
-for mxid in mxids:
-    print mxid, matrix.create_account(homeserver_secret, mxid, passgen_secret)
+for mxid in args.mxids:
+    print mxid, matrix.create_account(args.homeserver_secret, mxid, args.passgen_secret)
